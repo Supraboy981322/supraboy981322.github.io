11aff193d432e6dab87a016814a9c37b4ca7a596
Supraboy981322 <97927259+Supraboy981322@users.noreply.github.com>
2023-08-11 14:27:22 -0500

    chatbot?

---
+++ scripts/chatbot-script.js
@@ -0,0 +1,31 @@
+$("#terminal").terminal(async function (command, terminal) {
+    try {
+        const prompt = `you are a helpful, knowledge sharing chatbot. I say: ${command}. You reply:`
+        const response = await fetch(
+            `https://api.openai.com/v1/completions`,
+            {
+                body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 256}),
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    Authorization: "Bearer [YOUR API KEY]",
+                },
+                    }
+        ).then((response) => {
+            if (response.ok) {
+                response.json().then((json) => {
+                    terminal.echo(json.choices[0].text.trim());
+                });
+            }
+        });
+      
+        console.log("Completed!");
+    } catch (err) { console.error(`Error: ${err}`) }
+},
+    {
+        greetings: 'GPT-3 Chatbot v0.1',
+        name: 'gpt3_demo',
+        height: 400,
+        width: 800,
+        prompt: '> '
+    });
