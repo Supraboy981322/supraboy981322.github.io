690195af31bc81590d2d44b2ce8763ef0a1f7b89
Supraboy981322 <97927259+Supraboy981322@users.noreply.github.com>
2023-11-14 11:53:22 -0600

    - added a new command - "hakr" command is still broken

--- proto-and-wip/vterm-break/script.js
+++ proto-and-wip/vterm-break/script.js
@@ -27,13 +27,84 @@ $('body').terminal({
             this.echo(
                 'Err. no command for ' + what);
         }
-    }, 
-    hakr: function() {
-        let syntax = ["1","2","3","4","5","6","7","8","9","10"];
-        var ranNum = Math.round(Math.random() * (syntax.length - 1) + 1);
-        this.echo('> ' + syntax[ranNum]);
-        
     },
+    hakr: function () {
+
+        
+            let syntax = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+      
+            var modules = [
+                "axios", "lodash", "sentry-cli", "babel", "nodemon", "mongoose", "underscore", "async", "request", "lodash", "commander", "express", "optimist", "colors", "coffee-script",
+                "mkdirp", "debug", "q", "chalk", "yeoman-generator", "moment", "glob", "through2", "jade", "uglify-js",
+                "socket.io", "gulp-util", "redis", "cheerio", "through", "node-uuid", "connect", "winston", "mime",
+                "minimist", "bluebird", "grunt", "handlebars", "mongodb", "rimraf", "semver", "ejs", "mongoose", "marked",
+                "xml2js", "lodash.string", "fs-extra", "mocha", "js-yaml", "superagent", "less", "extend", "esprima",
+                "jquery", "stylus", "body-parser", "xtend", "jsdom", "event-stream", "shelljs", "minimatch", "prompt",
+                "browserify", "wrench", "ws", "mysql", "readable-stream", "yosay", "inherits", "when", "pkginfo",
+                "backbone", "nopt", "cli-color", "concat-stream", "passport", "nodemailer", "gulp", "chai", "inquirer",
+                "nconf", "validator", "yargs", "mustache", "qs", "clean-css", "npm", "ncp", "should", "open", "aws-sdk",
+                "graceful-fs", "temp", "http-proxy", "iconv-lite", "requirejs", "socket.io-client", "hiredis", "uuid",
+                "promise", "escodegen", "bower", "oauth", "log4js", "cli-table"
+            ];
+
+      var ranNum = Math.round(Math.random() * (syntax.length - 1) + 1);
+            var senteces = [
+                "it worked if it ends with ok",
+                "cli [ 'node', '" + "vTerm-Break By Lunch Break Software" + "', 'install', '--verbose' ]",
+                "using npm@1.4.28 " + "vTerm-Break By Lunch Break Software",
+                "using node@v0.10.32",
+                "readDependencies using package.json deps",
+                "install where, deps " + "vTerm-Break By Lunch Break Software" + ", [ '" + modules[ranNum] + "' ] ]",
+                "readDependencies using package.json deps",
+                "already installed skipping  " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "already installed skipping boganipsum@0.1.0 " + "vTerm-Break By Lunch Break Software",
+                "build /Users/samuel/Documents/bebusy",
+                "linkStuff [ false, false, false, '/Users/samuel/Documents' ]",
+                "rebuildBundles " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "rebuildBundles [ '.bin', 'boganipsum', 'colors' ]",
+                "install " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "postinstall " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "prepublish " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "preinstall " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "linkStuff " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "linkBins " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "linkMans " + modules[ranNum] + "@" + Math.random() * (syntax.length - 1) + 1,
+                "exit [ 0, true ]",
+                "ok",
+                "etag " + getHexNumber()
+            ];
+            var otherModules = [
+                "npm", "install", "download", "parse", "ok", "verb", "WARN", "info"
+            ];
+            var allColors = [
+                "100 Continue", "101 Switching Protocols", "102 Processing", "200 OK", "201 Created", "202 Accepted",
+                "203 Non-Authoritative Information", "204 No Content", "205 Reset Content", "206 Partial Content",
+                "207 Multi-Status", "208 Already Reported", "226 IM Used (RFC 3229)", "300 Multiple Choices",
+                "301 Moved Permanently", "302 Found", "303 See Other", "304 Not Modified", "305 Use Proxy",
+                "306 Switch Proxy", "307 Temporary Redirect", "308 Permanent Redirect", "prepublish", "postinstall",
+                "install", "rebuildBundles", "linkMans", "linkBins", "linkStuff", "install", "about to build", "addNamed",
+                "lock", "etag", "parsed url", "search", "query", "host", "auth", "slashes", "cache add", "GET", "POST",
+                "trying", "installOne", "tar unpack"
+            ];
+
+            function getHexNumber() {
+                var stringSize = 60;
+                var generatedHexString = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                for (var i = 0; i < stringSize; i++) {
+                    generatedHexString += possible.charAt(Math.floor(Math.random() * possible.length));
+                }
+                return generatedHexString;
+            }
+            this.echo('> ' + modules[ranNum]);
+            this.echo(senteces[ranNum]);
+            this.echo('>' + otherModules[ranNum]);
+            this.echo(allColors[ranNum]);
+            this.echo('>' + syntax[ranNum]);
+    },
+    echo: function (what) {
+        this.echo(what);
+    }
 }, {
     greetings: greetings4.innerHTML
 });
