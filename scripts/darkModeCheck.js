af892ef03aa576a561c0bdcaa21df132d50575e0
Supraboy981322 <97927259+Supraboy981322@users.noreply.github.com>
2025-04-08 10:44:20 -0500

    Created separate stylesheets for color and created darkMode stylesheets

--- scripts/darkModeCheck.js
+++ scripts/darkModeCheck.js
@@ -2,7 +2,7 @@ function checkDarkMode() {
     let params = new URLSearchParams(document.location.search);
     let darkMode = params.get("d");
     if (darkMode != null || darkMode) {
-      document.getElementById("style").setAttribute("href", "/styles/indexDark.css");
+      document.getElementById("style").setAttribute("href", "/styles/styleDark.css");
     }
   }
   addEventListener(window.onload, checkDarkMode());
