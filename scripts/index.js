ea3132fd006b4444bf141958680c9c72f9874425
Supraboy981322 <97927259+Supraboy981322@users.noreply.github.com>
2025-04-28 13:32:57 -0500

    woops

--- scripts/index.js
+++ scripts/index.js
@@ -86,25 +86,25 @@ addEventListener(window.onload, loadIframe());
 
 
 
-/* scriptSet 3 */
-
-//define the location of the favicon element
-const faviconLoc = document.getElementById("favicon");
-//define the directory for the favicons
-const faviconDir = "/img/favicon/";
-//define the file extension for the favicons
-const faviconFileExtension = ".png";
-//define the names of the favicons
-const faviconNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41"];
-//create a tick variable for the favicon
-var faviconTick = 0;
-//onload, change the favicon every 83.33ms (12fps), to the next item in the list
-window.onload = () => { setInterval(faviconInterval(), 83.33) };
-
-function faviconInterval() {
-    faviconLoc.setAttribute("href", (faviconDir + faviconNames[faviconTick] + faviconFileExtension));
-    faviconTick++;
-    if (favicon = faviconNames.length) {
-        faviconTick = 0;
-    };
-}
+///* scriptSet 3 */
+//
+////define the location of the favicon element
+//const faviconLoc = document.getElementById("favicon");
+////define the directory for the favicons
+//const faviconDir = "/img/favicon/";
+////define the file extension for the favicons
+//const faviconFileExtension = ".png";
+////define the names of the favicons
+//const faviconNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41"];
+////create a tick variable for the favicon
+//var faviconTick = 0;
+////onload, change the favicon every 83.33ms (12fps), to the next item in the list
+//window.onload = () => { setInterval(faviconInterval(), 83.33) };
+//
+//function faviconInterval() {
+//    faviconLoc.setAttribute("href", (faviconDir + faviconNames[faviconTick] + faviconFileExtension));
+//    faviconTick++;
+//    if (favicon = faviconNames.length) {
+//        faviconTick = 0;
+//    };
+//}
