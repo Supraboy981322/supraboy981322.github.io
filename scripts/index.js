19e70a813e1fb7da6b66aff5132e318573b662c7
Supraboy981322 <97927259+Supraboy981322@users.noreply.github.com>
2025-04-28 13:16:38 -0500

    Nevermind, that wasn't the problem

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,5 @@
 /* scriptSet 1 */
 
-//define the location of the favicon element
-const faviconLoc = document.getElementById("favicon");
-//define the directory for the favicons
-const faviconDir = "/img/favicon/";
-//define the file extension for the favicons
-const faviconFileExtension = ".png";
-//define the names of the favicons
-const faviconNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41"];
-
-//create a tick variable for the favicon
-var faviconTick = 0;
-
-//onload, change the favicon every 200ms (12fps), to the next item in the list
-window.onload(() => {
-    window.setInterval(() => {
-        faviconLoc.setAttribute("href", (faviconDir + faviconNames[faviconTick] + faviconFileExtension));
-        faviconTick++;
-        if (favicon = faviconNames.length) {
-            faviconTick = 0;
-        };
-    }, 200);
-});
-
-
-
-/* scriptSet 2 */
-
   //define the location of the sideNav
   const sideNav = document.getElementById("mySidenav");
   //define the location of the sidebarButton
@@ -63,7 +36,7 @@ window.onload(() => {
 
 
 
-/* scriptSet 3 */
+/* scriptSet 2 */
 
 //prevent arrow and space keys from scrolling page
 window.addEventListener("keydown", function (e) {
@@ -110,3 +83,28 @@ function checkDarkMode() {
 
 addEventListener(document.getElementById("pageContentFrame").onload, checkDarkMode());
 addEventListener(window.onload, loadIframe());
+
+
+
+/* scriptSet 3 */
+
+//define the location of the favicon element
+const faviconLoc = document.getElementById("favicon");
+//define the directory for the favicons
+const faviconDir = "/img/favicon/";
+//define the file extension for the favicons
+const faviconFileExtension = ".png";
+//define the names of the favicons
+const faviconNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41"];
+//create a tick variable for the favicon
+var faviconTick = 0;
+//onload, change the favicon every 200ms (12fps), to the next item in the list
+window.onload(() => {
+    window.setInterval(() => {
+        faviconLoc.setAttribute("href", (faviconDir + faviconNames[faviconTick] + faviconFileExtension));
+        faviconTick++;
+        if (favicon = faviconNames.length) {
+            faviconTick = 0;
+        };
+    }, 200);
+});
